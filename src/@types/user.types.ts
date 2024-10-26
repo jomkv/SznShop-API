f80059d21aec8745953b2ed21f7e49798132680d
Jom Karlo Verzosa <jomkarlov@gmail.com>
2024-10-26 17:11:33 +0800

    refactor: type renaming

--- src/@types/user.types.ts
+++ src/@types/user.types.ts
@@ -1,6 +1,6 @@
 import { Document } from "mongoose";
 
-export interface IUser extends Document {
+export interface IUserDocument extends Document {
   _id: string;
   id?: string;
   email: string;
