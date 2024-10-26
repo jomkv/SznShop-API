1df8176b7a50c92d08aa61dc5fc80d887d1f00ac
Jom Karlo Verzosa <jomkarlov@gmail.com>
2024-10-26 12:32:40 +0800

    feat: login route for dev

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import User from "../models/User";
+import { IUser } from "../@types/user.types";
+import BadRequestError from "../errors/BadRequestError";
 dotenv.config();
 
 // * Middlewares
 import passport from "passport";
-
-// * Controllers
-import { IUser } from "../@types/user.types";
+import asyncHandler from "express-async-handler";
 
 const router = Router();
 
@@ -16,6 +17,27 @@ router.route("/login").get(
     scope: ["profile", "email"],
   })
 );
+
+if (process.env.NODE_ENV === "development") {
+  router.route("/login-dev").post(
+    asyncHandler(async (req, res) => {
+      const user = await User.findOne({ email: req.body.email });
+
+      if (!user) {
+        res.status(404);
+        throw new BadRequestError("User not found");
+      }
+
+      const token = jwt.sign(
+        { userId: user._id, role: user.role },
+        process.env.JWT_SECRET as string,
+        { expiresIn: "1h" }
+      );
+      res.cookie("x-auth-cookie", token);
+      res.status(200).json({ message: "Logged in" });
+    })
+  );
+}
 
 router.route("/redirect").get(
   passport.authenticate("google", {
