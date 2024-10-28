750b6839112890877d50937da1cc6dae381bfa87
Jom Karlo Verzosa <jomkarlov@gmail.com>
2024-10-28 18:43:11 +0800

    refactor: check param IDs now work for endpoints with multiple and differently named params

--- src/middlewares/objectId.middleware.ts
+++ src/middlewares/objectId.middleware.ts
@@ -3,20 +3,28 @@ import { isValidObjectId } from "mongoose";
 
 /**
  * Used for routes that have MongoDB IDs as params.
- * This validates the given param ID.
+ * This validates ALL given param ID.
  *
  * @param {import('express').Request} req - The Express request object.
  * @param {import('express').Response} res - The Express response object.
  * @param {import('express').NextFunction} next - The Express next middleware function.
- * @throws {Error} Throws an error if param ID is not a valid MongoDB ID
+ * @throws {Error} Throws an error if any param ID is not a valid MongoDB ID
  */
-const checkObjectId = (req: Request, res: Response, next: NextFunction) => {
-  if (!isValidObjectId(req.params.id)) {
+const checkParamIds = (req: Request, res: Response, next: NextFunction) => {
+  // iterate through params and get invalid IDs if any
+  const invalidIds = Object.entries(req.params).filter(
+    ([_, value]) => !isValidObjectId(value)
+  );
+
+  if (invalidIds.length > 0) {
+    const invalidIdMessages = invalidIds.map(
+      ([key, value]) => `Invalid ObjectId for parameter [${key}]: ${value}`
+    );
     res.status(400);
-    throw new Error(`Invalid ObjectId of: ${req.params.id}`);
+    throw new Error(invalidIdMessages.join(", "));
   }
 
   next();
 };
 
-export default checkObjectId;
+export default checkParamIds;
