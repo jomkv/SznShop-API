13b97bfaf152d55dd5e115e8f5a9cffeeff9143c
Jom Karlo Verzosa <jomkarlov@gmail.com>
2024-11-04 13:21:30 +0800

    feat: added virtual prop of stocks for products

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -3,30 +3,46 @@ import imageSchema from "./schemas/image.schema";
 import { IProductDocument } from "../@types/product.types";
 import { softDeletePlugin } from "./plugins/softDelete.plugin";
 
-const productSchema: Schema = new Schema<IProductDocument>({
-  name: {
-    type: String,
-    required: true,
+const productSchema: Schema = new Schema<IProductDocument>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    images: {
+      type: [imageSchema],
+      default: [],
+    },
+    active: {
+      type: Boolean,
+      default: true,
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  images: {
-    type: [imageSchema],
-    default: [],
-  },
-  active: {
-    type: Boolean,
-    default: true,
-  },
-});
+  { toJSON: { virtuals: true } }
+);
 
 softDeletePlugin(productSchema);
+
+productSchema.virtual("stocks", {
+  ref: "Stocks",
+  localField: "_id",
+  foreignField: "productId",
+  justOne: true,
+});
+
+// pre hook to populate
+productSchema.pre(["find", "findOne"], function (next) {
+  this.populate({ path: "stocks" });
+  next();
+});
 
 const Product = model<IProductDocument>("Product", productSchema);
 
