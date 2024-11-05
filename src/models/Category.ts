984242cb721af1ebf87db4b1796f456fcd6130d4
Jom Karlo Verzosa <jomkarlov@gmail.com>
2024-11-05 21:47:59 +0800

    feat: productCount virtual prop for Category

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,15 +1,35 @@
 import { Schema, model } from "mongoose";
 import { ICategoryDocument } from "../@types/product.types";
 
-const categorySchema: Schema = new Schema<ICategoryDocument>({
-  name: {
-    type: String,
-    required: true,
+const categorySchema: Schema = new Schema<ICategoryDocument>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    showInMenu: {
+      type: Boolean,
+      default: false,
+    },
   },
-  showInMenu: {
-    type: Boolean,
-    default: false,
-  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+categorySchema.virtual("productCount", {
+  ref: "CategoryProduct",
+  localField: "_id",
+  foreignField: "categoryId",
+  count: true,
+});
+
+// pre hook to populate
+categorySchema.pre(["find", "findOne"], function (next) {
+  this.populate({ path: "productCount" });
+  next();
 });
 
 const Category = model<ICategoryDocument>("Category", categorySchema);
