986219e86700317c6aa10ec4a585d52d9d6e4513
Jom Karlo Verzosa <jomkarlov@gmail.com>
2024-12-13 23:33:14 +0800

    feat: prehook for updating cart items when stocks is changed

--- src/models/Stocks.ts
+++ src/models/Stocks.ts
@@ -1,5 +1,6 @@
 import mongoose, { Schema, model } from "mongoose";
-import { IStocksDocument } from "../@types/product.types";
+import { IStocksDocument, Size } from "../@types/product.types";
+import CartProduct from "./CartProduct";
 
 const stocksSchema: Schema = new Schema<IStocksDocument>({
   productId: {
@@ -31,6 +32,27 @@ const stocksSchema: Schema = new Schema<IStocksDocument>({
     type: Number,
     default: 0,
   },
+});
+
+stocksSchema.pre("save", async function (next) {
+  const stocks = this as unknown as IStocksDocument;
+  const cartItems = await CartProduct.find({ productId: stocks.productId });
+  const session = stocks.$session();
+
+  for (const cartItem of cartItems) {
+    const size = cartItem.size as Size;
+    const availableStock = stocks[size];
+
+    if (availableStock > 0) {
+      cartItem.quantity = availableStock;
+      await cartItem.save({ session: session || undefined });
+    } else {
+      // Delete the cart item if no stock is available
+      await cartItem.deleteOne({ session: session || undefined });
+    }
+  }
+
+  next();
 });
 
 const Stocks = model<IStocksDocument>("Stocks", stocksSchema);
