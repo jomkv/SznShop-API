73fb427e542a31b77f16489c8f2693faa056026a
Jom Karlo Verzosa <jomkarlov@gmail.com>
2025-01-28 04:09:05 +0800

    feat: edit profile

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -8,6 +8,7 @@ import User from "../models/User";
 import BadRequestError from "../errors/BadRequestError";
 import DatabaseError from "../errors/DatabaseError";
 import AuthenticationError from "../errors/AuthenticationError";
+import { IUserDocument, IUserToken } from "../@types/user.types";
 
 // @desc    Get users
 // @route   GET /api/user?query=role
@@ -19,6 +20,78 @@ const getUsers = asyncHandler(
     const users = await (role ? User.find({ role }) : User.find());
 
     res.status(200).json({ message: "Users fetched", users });
+  }
+);
+
+// @desc    Get usernames and emails
+// @route   GET /api/user/username-name
+// @access  User & Admin
+const getUsernameAndName = asyncHandler(
+  async (req: Request, res: Response): Promise<any> => {
+    const users = await User.find();
+
+    const usernames = users.map((user) => user.username);
+    const names = users.map((user) => user.displayName);
+
+    res
+      .status(200)
+      .json({ message: "Usernames and Names fetched", usernames, names });
+  }
+);
+
+// @desc    Edit profile
+// @route   PUT /api/user
+// @access  User & Admin
+const editProfile = asyncHandler(
+  async (req: Request, res: Response): Promise<any> => {
+    const { username, fullName } = req.body;
+
+    if (!username || !fullName) {
+      throw new BadRequestError("Incomplete Input");
+    }
+
+    const isTaken = await User.findOne({
+      username,
+    });
+
+    if (isTaken) {
+      throw new BadRequestError("Username taken");
+    }
+
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.sznUser?.userId,
+        {
+          username: username,
+          displayName: fullName,
+        },
+        { new: true }
+      );
+
+      res.status(200).json({
+        message: "Profile updated",
+        updatedUser,
+      });
+    } catch (error) {
+      throw new DatabaseError();
+    }
+  }
+);
+
+// @desc    Get current user
+// @route   GET /api/user/me
+// @access  Private
+const getMe = asyncHandler(
+  async (req: Request, res: Response): Promise<any> => {
+    const userToken = req.sznUser as IUserToken;
+    const user: IUserDocument | null = await User.findById(userToken.userId);
+
+    if (!user) {
+      res.status(404);
+      throw new BadRequestError("User not found");
+    }
+
+    res.status(200).json({ user });
   }
 );
 
@@ -45,4 +118,4 @@ const banUser = asyncHandler(
   }
 );
 
-export { getUsers, banUser };
+export { getUsers, getUsernameAndName, editProfile, banUser, getMe };
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,15 +1,23 @@
 import { Router } from "express";
 
 // * Middlewares
-import { adminProtect } from "../middlewares/auth.middleware";
+import { adminProtect, protect } from "../middlewares/auth.middleware";
 import checkParamIds from "../middlewares/objectId.middleware";
 
 // * Controllers
-import { getUsers, banUser } from "../controllers/user.controller";
+import {
+  getUsers,
+  getUsernameAndName,
+  editProfile,
+  banUser,
+  getMe,
+} from "../controllers/user.controller";
 
 const router = Router();
 
-router.route("/").get(adminProtect, getUsers);
+router.route("/").get(adminProtect, getUsers).put(protect, editProfile);
+router.route("/me").get(protect, getMe);
+router.route("/username-name").get(protect, getUsernameAndName);
 router.route("/:id").post(checkParamIds, adminProtect, banUser);
 
 export default router;
