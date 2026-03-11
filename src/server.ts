f56376b238c2fdb05c85e43312fc9251a35729c9
Jom Karlo Verzosa <jomkarlov@gmail.com>
2026-03-11 17:19:58 +0800

    fix: please man

--- src/server.ts
+++ src/server.ts
@@ -31,7 +31,7 @@ const port = process.env.PORT || 3000;
 
 // * Middlewares
 app.set("trust proxy", 1);
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.urlencoded({ extended: false })); // allow destructuring of req.body
 app.use(express.json());
 app.use(cookieParser());
